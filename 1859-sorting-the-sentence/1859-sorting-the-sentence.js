400b53c986289d8cae7c253e040a73861de53013
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-14 00:56:05 -0700

    Time: 55 ms (87.36%), Space: 41.3 MB (96.57%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.js
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -5,9 +5,10 @@
 var sortSentence = function(s) {
     let str=s.split(' ')
     let words=new Array(str.length)
+    let word,index
     for(let i=0;i<str.length;i++){
-        let word=str[i]
-        let index=word[word.length-1]
+        word=str[i]
+        index=word[word.length-1]
         words[index-1]=word.slice(0,-1)   
     }
     return words.join(' ')
