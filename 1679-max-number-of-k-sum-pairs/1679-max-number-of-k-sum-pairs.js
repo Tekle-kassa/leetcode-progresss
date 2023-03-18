e0e9846680f40710e90ce6a4043ebd9268898e1b
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-18 07:52:56 -0700

    Time: 201 ms (21.51%), Space: 54.6 MB (52.69%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function(nums, k) {
+    nums.sort((num1,num2)=>num1-num2)
+    console.log(nums)
+    let i=0
+    let j=nums.length-1
+    let counter=0
+    while(i<j){
+        if(nums[i]>k){
+            return counter
+        }
+        if(nums[i]+nums[j]===k){
+            counter++
+            i++
+            j--
+        }else if(nums[i]+nums[j]>k){
+            j--
+        }else{
+            i++
+        }
+    }
+    return counter
+};
