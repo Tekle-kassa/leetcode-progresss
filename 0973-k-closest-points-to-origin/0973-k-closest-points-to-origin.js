5bae6056516d872b902e425a6abcdc1d9ae72be0
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-18 06:03:25 -0700

    Time: 214 ms (70.08%), Space: 50.8 MB (99.80%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
@@ -4,9 +4,6 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-    points.sort(numCompare)
+   points.sort((num1,num2)=>(num1[0]**2+num1[1]**2)-(num2[0]**2+num2[1]**2))
    return points.slice(0,k)
 };
-function numCompare(num1,num2){
-    return (num1[0]**2+num1[1]**2)-(num2[0]**2+num2[1]**2)
-}
