a296db2a909c10e45a29f699a298d28ab15aca20
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-19 01:37:45 -0700

    Time: 84 ms (59.72%), Space: 48.3 MB (5.25%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let i=0
+    let j=nums.length-1
+    let temp=[]
+    let obj1={}
+    let ans=[]
+    for(let k=0;k<nums.length;k++){
+        if(!obj1[nums[k]]){
+            let arr2=[]
+            arr2.push(k)
+            obj1[nums[k]]=arr2    
+        }else{
+            obj1[nums[k]].push(k)
+        }
+        
+    }
+    nums.sort(numCompare)
+    let store=[]
+    while(i<j){
+        if(nums[i]+nums[j]===target){
+            store.push(nums[i])
+            store.push(nums[j])
+            break
+        }else if(nums[i]+nums[j]<target){
+            i++
+        }else j --
+    }
+    for(let k=0;k<2;k++){
+        let idx=obj1[store[k]].pop()
+        ans.push(idx)
+    }
+    return ans
+};
+function numCompare(num1,num2){
+       return num1-num2
+}
