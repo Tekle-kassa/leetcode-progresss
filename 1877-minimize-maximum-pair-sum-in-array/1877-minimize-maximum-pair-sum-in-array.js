a3c8e644593328d09d2044a8c7f56cd3709cf75b
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-19 06:04:52 -0700

    Time: 355 ms (88.65%), Space: 56.5 MB (94.33%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.js
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.js
@@ -6,11 +6,13 @@ var minPairSum = function(nums) {
     nums.sort((num1,num2)=>num1-num2)
     let i=0
     let j=nums.length-1
-    let temp=[]
+    let res=0
     while(i<j){
-        temp.push(nums[i]+nums[j])
+        if(nums[i]+nums[j]>res){
+            res=nums[i]+nums[j]
+        }
         i++
         j--
     }
-    return Math.max(...temp)
+    return res
 };
