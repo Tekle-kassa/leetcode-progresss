269db514c540205eb6a3b124e989be2fc41c43c6
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-22 00:40:13 -0700

    Time: 317 ms (24.41%), Space: 76.9 MB (9.15%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -12,9 +12,7 @@ var containsNearbyDuplicate = function(nums, k) {
           obj[nums[i]].push(i)
       }
   }
-    // console.log(obj)
     nums.sort((num1,num2)=>num1-num2)
-    // console.log(nums)
     for(let i=0;i<nums.length;i++){
         if(nums[i]===nums[i+1]){
             for(let j=0;j<obj[nums[i]].length;j++){
