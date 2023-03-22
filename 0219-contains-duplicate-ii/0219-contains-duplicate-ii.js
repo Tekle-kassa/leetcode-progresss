652b25e255cf098eecc80568884e42be8e59d802
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-22 00:39:33 -0700

    Time: 327 ms (24.18%), Space: 76.6 MB (9.50%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -5,24 +5,24 @@
  */
 var containsNearbyDuplicate = function(nums, k) {
     let obj={}
+  for(let i=0;i<nums.length;i++){
+      if(!obj[nums[i]]){
+          obj[nums[i]]=[i]
+      }else{
+          obj[nums[i]].push(i)
+      }
+  }
+    // console.log(obj)
+    nums.sort((num1,num2)=>num1-num2)
+    // console.log(nums)
     for(let i=0;i<nums.length;i++){
-        if(!obj[nums[i]]){
-            obj[nums[i]]=[i]
-        }else{
-            obj[nums[i]].push(i)
-        }    
-    }
-     // console.log(obj)
-    for(let i in obj){
-        // console.log(obj[i])
-        if(obj[i].length>1){
-            for(let j=0;j<obj[i].length;j++){
-                if(obj[i][j+1]-obj[i][j]<=k){
+        if(nums[i]===nums[i+1]){
+            for(let j=0;j<obj[nums[i]].length;j++){
+                if(Math.abs(obj[nums[i]][j+1]-obj[nums[i]][j])<=k){
                     return true
                 }
             }
-        } 
+        }        
     }
     return false
-   
 };
