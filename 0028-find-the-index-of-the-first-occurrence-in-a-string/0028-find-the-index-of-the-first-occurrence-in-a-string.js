c1161455093fbd2c531777ee4b4a6157a83139c7
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-23 05:25:19 -0700

    Time: 60 ms (58.71%), Space: 43.7 MB (6.57%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -7,9 +7,6 @@ var strStr = function(haystack, needle) {
     for(let i=0;i<haystack.length;i++){
         let s=''
         let j=i
-        // for(let j=i;s.length>=needle.length;j++){
-        //     s+=haystack[j]
-        // }
         while(s.length<needle.length){
             s+=haystack[j]
             j++
