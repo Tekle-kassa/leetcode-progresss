4b1d50deae3b07661da047e6a7480e1d47294318
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-23 13:04:31 -0700

    Time: 68 ms (75.62%), Space: 44.4 MB (24.76%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -3,8 +3,7 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-  let j=2;
-    
+    let j=2;
     for(let i=2;i<nums.length;i++){
         if(nums[i]!==nums[j-2]){
             nums[j]=nums[i];
