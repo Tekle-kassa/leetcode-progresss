ac0c122753b256d5b9d8dbe13bac0555209f20ef
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-24 04:44:44 -0700

    Time: 637 ms (59.46%), Space: 26.8 MB (24.82%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -1,17 +1,10 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         piles.sort()
-        freq=int(len(piles)/3)
-        i=0
-        j=len(piles)-1
-        counter=0
-        for k in range(freq):
-            temp=[]
-            temp.append(piles[j])
-            temp.append(piles[j-1])
-            temp.append(piles[i])
-            counter+=temp[1]
-            j-=2
-            i+=1
-        return counter
+        i=len(piles)-2
+        res=0
+        while i>=len(piles)/3:
+            res+=piles[i]
+            i-=2
+        return res
         
