3f14239c327a6bb59441989d578b46075bc701ec
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-03-26 12:15:14 -0700

    Time: 335 ms (10.88%), Space: 76.5 MB (18.37%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
@@ -4,7 +4,6 @@
  */
 var minSetSize = function(arr) {
     let freq=[]
-    let mySet=new Set()
     let obj={}
     for(let i=0;i<arr.length;i++){
         if(!obj[arr[i]]){
@@ -13,17 +12,14 @@ var minSetSize = function(arr) {
             obj[arr[i]].push(i)
         }
     }
-    // console.log(obj)
     for(let i in obj){
         freq.push([obj[i].length,parseInt(i)])
     }
      freq.sort((num1,num2)=>num2[0]-num1[0])
-    // console.log(freq)
     let j=0
     let len=0
     let count=0
     while(len<arr.length/2){
-        // console.log(freq[j][0])
        len+=freq[j][0]
         count++
         j++
