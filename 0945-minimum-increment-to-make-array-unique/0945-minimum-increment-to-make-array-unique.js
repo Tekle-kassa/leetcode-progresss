ea66c6e2d057add23764834220e2b3903feffd6e
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-04-01 01:02:48 -0700

    Time: 267 ms (69.39%), Space: 53.3 MB (60.20%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.js
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.js
@@ -7,12 +7,11 @@ var minIncrementForUnique = function(nums) {
     let counter=0
     let i=0
     while(i<nums.length-1){
-        if(nums[i]<nums[i+1]){
-            i++
-        }else{
-            nums[i+1]+=1
-            counter++
+        if(nums[i]>=nums[i+1]){
+            counter+=nums[i]-nums[i+1]+1
+            nums[i+1]+=nums[i]-nums[i+1]+1
         }
+        i++
     }
     return counter
 };
