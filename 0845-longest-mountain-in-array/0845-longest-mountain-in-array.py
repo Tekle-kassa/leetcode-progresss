634f24e5cc4da118e0e9ce19fcde45f4c1cd9a87
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-04-06 01:52:52 -0700

    Time: 165 ms (86.12%), Space: 15.1 MB (75.04%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -6,14 +6,14 @@ class Solution:
         while i<=n-2:
             if arr[i]>arr[i-1] and arr[i]>arr[i+1]:
                 j=i
-                count=1
+                # count=1
                 while j>0 and arr[j]>arr[j-1]:
                     j-=1
-                    count+=1
+                    # count+=1
                 while i<n-1 and arr[i]>arr[i+1]:
                     i+=1
-                    count+=1
-                ans=max(ans,count)
+                    # count+=1
+                ans=max(ans,i-j+1)
             else:
                 i+=1
         return ans
