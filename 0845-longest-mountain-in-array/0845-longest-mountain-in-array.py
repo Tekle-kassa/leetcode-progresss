aecfcf30cc78d21537d0d2889709d48913ebc6d0
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-04-06 01:52:08 -0700

    Time: 173 ms (64.12%), Space: 15.1 MB (75.04%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -1,41 +1,19 @@
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
-        i=0
-        j=1
-        count=0
-        if len(arr)<3:
-            return 0
-        while arr[i]>=arr[j]:
-            i+=1
-            j+=1
-            if j==len(arr):
-                break
-        while j+1<len(arr):
-            while j<len(arr) and arr[i]>=arr[j]:
-                i+=1
-                j+=1
-            if j==len(arr):
-                break
-            if j+1<len(arr)and arr[j]<arr[j+1]:
-                j+=1
-                if j==len(arr):
-                    break
-                continue
-            elif j+1<len(arr)and arr[j]>arr[j+1]:
-                while arr[j]>arr[j+1]:
-                    j+=1
-                    if j+1==len(arr):
-                        break
-                count=max(count,j-i+1)
-                i=j
-                j+=1
+        n=len(arr)
+        i=1
+        ans=0
+        while i<=n-2:
+            if arr[i]>arr[i-1] and arr[i]>arr[i+1]:
+                j=i
+                count=1
+                while j>0 and arr[j]>arr[j-1]:
+                    j-=1
+                    count+=1
+                while i<n-1 and arr[i]>arr[i+1]:
+                    i+=1
+                    count+=1
+                ans=max(ans,count)
             else:
-                i=j+1
-                j+=2
-                continue
-            count=max(count,j-i+1)
-            # print(count)
-            # print(arr[i])
-            # print(arr[j])
-        return count
-            
+                i+=1
+        return ans
