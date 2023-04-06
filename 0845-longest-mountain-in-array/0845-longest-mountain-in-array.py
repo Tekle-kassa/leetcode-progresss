57b8e47591ea491e0f7b30d1e4870f03455c1efa
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-04-06 02:10:21 -0700

    Time: 172 ms (66.30%), Space: 15.2 MB (75.04%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -1,19 +1,29 @@
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
-        n=len(arr)
-        i=1
-        ans=0
-        while i<=n-2:
-            if arr[i]>arr[i-1] and arr[i]>arr[i+1]:
-                j=i
-                # count=1
-                while j>0 and arr[j]>arr[j-1]:
-                    j-=1
-                    # count+=1
-                while i<n-1 and arr[i]>arr[i+1]:
-                    i+=1
-                    # count+=1
-                ans=max(ans,i-j+1)
+        n = len(arr)
+        ans = 0
+
+        i = 1
+        while i < n - 1:
+            # Check if current element is the starting point of a mountain
+            if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
+                left, right = i, i
+
+                # Expand mountain to the left
+                while left > 0 and arr[left] > arr[left - 1]:
+                    left -= 1
+
+                # Expand mountain to the right
+                while right < n - 1 and arr[right] > arr[right + 1]:
+                    right += 1
+
+                # Check if a mountain is found
+                ans = max(ans, right - left + 1)
+
+                # Move to next element after the mountain
+                i = right
             else:
-                i+=1
+                # Move to next element
+                i += 1
+
         return ans
