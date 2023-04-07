26ab42d3f6e65a758fbfe82a7a83a4d0c24e3f2c
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-04-07 12:42:35 -0700

    Time: 56 ms (61.33%), Space: 13.9 MB (97.78%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.py
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -6,8 +6,6 @@ class Solution:
         res=0
         i=0
         j=len(tokens)-1
-        if len(tokens)==0 or power<tokens[0]:
-            return 0
         while i<=j:
             if power>=tokens[i]:
                 power-=tokens[i]
