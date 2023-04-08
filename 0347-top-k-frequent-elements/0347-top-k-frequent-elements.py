25404e3ae5ac601c84db2b20ca1c13c58bd95b77
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-04-08 13:17:43 -0700

    Time: 107 ms (57.02%), Space: 18.8 MB (42.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,10 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        obj=defaultdict(bool)
-        for i in range(len(nums)):
-            if not obj[nums[i]]:
-                obj[nums[i]]=[i]
-            else:
-                obj[nums[i]].append(i)
-        freq=[]
-        for i in obj:
-            freq.append([len(obj[i]),int(i)])
-        freq.sort(reverse=True)
-        j=0
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
+        temp=Counter(nums)
+        items=temp.items()
+        items=sorted(items,key=lambda x:x[1])
+        # print(items)
         res=[]
-        while len(res)<k:
-            res.append(freq[j][1])
-            j+=1
+        for i in range(k):
+            res.append(items.pop(len(items)-1)[0])
         return res
