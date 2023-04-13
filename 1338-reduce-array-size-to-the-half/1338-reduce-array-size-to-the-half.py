85c3ed105e3c9b76ef6522e6674acc69f9537d59
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-04-13 00:27:37 -0700

    Time: 551 ms (92.70%), Space: 32.3 MB (59.72%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,13 +1,11 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        # freq=[]
         obj=Counter(arr)
-        s_obj=sorted(obj.items(),reverse=True,key=lambda x:x[1])
-        l=0
-        j=0
-        count=0
+        obj=sorted(obj.values(),reverse=True)
+        l,count=0,0
         while l<len(arr)/2:
-            l+=s_obj[j][1]
+            l+=obj[count]
             count+=1
-            j+=1
         return count
+
+            
