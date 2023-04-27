c98eba2ec130cc65dd210d31304ceb54dd0f0bdf
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-04-27 06:47:20 -0700

    Time: 37 ms (25.93%), Space: 16.3 MB (15.17%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,30 +1,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s)%2!=0:
-            return False
-        if s[0]!='[' and s[0]!='{' and s[0]!='(':
-            return False
         res=[]
-        for i in range(len(s)):
-            if s[i]=='(' or s[i]=='[' or s[i]=='{':
-                res.append(s[i])
-            elif s[i]==')':
-                if len(res)>0 and res[len(res)-1]=='(':
-                    res.pop()
-                else:
-                    return False
-            elif s[i]==']':
-                if len(res)>0 and res[len(res)-1]=='[':
-                    res.pop()
-                else:
-                    return False
-            elif s[i]=='}':
-                if len(res)>0 and res[len(res)-1]=='{':
-                    res.pop()
-                else:
-                    return False
-        if len(res)==0:
-            return True
+        if s[0]==")" or s[0]=='}' or s[0]==']':
+            return False
+        if len(s)%2==0:
+            for i in range(len(s)):
+                if s[i]=='(' or s[i]=='[' or s[i]=='{':
+                    res.append(s[i])
+                elif s[i]==')':
+                    if len(res)>0 and res[len(res)-1]=='(':
+                        res.pop()
+                    else:return False
+                elif s[i]==']':
+                    if len(res)>0 and res[len(res)-1]=='[':
+                        res.pop()
+                    else:return False
+                elif s[i]=='}':
+                    if len(res)>0 and res[len(res)-1]=='{':
+                        res.pop()
+                    else:return False
+            if len(res)==0:
+                return True
         return False
-                
-            
