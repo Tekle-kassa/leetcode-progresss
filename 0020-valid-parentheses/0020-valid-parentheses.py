949eab0afcb47ac3c8c03c96383c9319ee966a86
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-05-19 12:10:49 +0300

    Time: 49 ms (15.84%), Space: 16.3 MB (11.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,24 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         res=[]
-        if s[0]==")" or s[0]=='}' or s[0]==']':
-            return False
-        if len(s)%2==0:
-            for i in range(len(s)):
-                if s[i]=='(' or s[i]=='[' or s[i]=='{':
-                    res.append(s[i])
-                elif s[i]==')':
-                    if len(res)>0 and res[len(res)-1]=='(':
-                        res.pop()
-                    else:return False
-                elif s[i]==']':
-                    if len(res)>0 and res[len(res)-1]=='[':
-                        res.pop()
-                    else:return False
-                elif s[i]=='}':
-                    if len(res)>0 and res[len(res)-1]=='{':
-                        res.pop()
-                    else:return False
-            if len(res)==0:
-                return True
+        obj={')':'(',']':'[','}':'{'}
+        for p in s:
+            if p in obj: 
+                if res and res[len(res)-1]==obj[p]:
+                    res.pop()
+                else:return False
+            else:
+                res.append(p)
+        if not res:
+            return True
         return False
+            
+                
+        
