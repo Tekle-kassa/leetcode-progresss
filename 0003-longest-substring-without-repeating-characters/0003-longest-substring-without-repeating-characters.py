71b8bfa4819e00374ccf3152ea70d15b65a81bfc
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-05-23 22:54:38 +0300

    Time: 71 ms (59.79%), Space: 16.5 MB (15.63%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -6,18 +6,14 @@ class Solution:
             temp+=s[0]
             count=1
             while r<len(s):
-                # print(temp)
-                
                 if s[r] in temp:
                     i=0
                     while s[r]!=temp[i]:
                         i+=1
                     temp=temp[i+1:]
-                    # print(temp)
                     temp+=s[r]
                 else:
                     temp+=s[r]
-                    # print(temp)
                 r+=1
                 count=max(count,len(temp))
             return count
