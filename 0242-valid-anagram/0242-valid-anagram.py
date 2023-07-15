b44126114140d06182ad3fce37ec61de3b957075
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-07-15 17:15:21 +0300

    Time: 51 ms (95.54%), Space: 16.8 MB (64.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,20 +2,6 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        temp1=defaultdict(int)
-        temp2=defaultdict(int)
-        for i in range(len(s)):
-            if not temp1[s[i]]:
-                temp1[s[i]]=1
-            else:
-                temp1[s[i]]+=1
-            if not temp2[t[i]]:
-                temp2[t[i]]=1
-            else:
-                temp2[t[i]]+=1
-        # temp1=Counter(s)
-        # temp2=Counter(t)
-        for c in temp1:
-            if temp1[c]!=temp2[c]:
-                return False
-        return True
+        temp1=Counter(s)
+        temp2=Counter(t)
+        return temp1==temp2
