4932e266587bbcee4da85d5b6f30dcf7af3820b3
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-10-20 17:09:14 +0300

    Time: 46 ms (22.07%), Space: 43.8 MB (23.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[]res=new int[2];
+        outerLoop:
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
                     res[0]=i;
                     res[1]=j;
-                    break;
+                    break outerLoop;
                 }
             }
         }
