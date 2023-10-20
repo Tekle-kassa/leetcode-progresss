440f56653407a3c572882e072e3e295643e56f95
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-10-20 19:59:37 +0300

    Time: 10 ms (56.56%), Space: 44.9 MB (8.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,38 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[]res=new int[2];
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    res[0]=i;
-                    res[1]=j;
-                    return res;
-                }
+        Map<Integer, List<Integer>> obj = new HashMap<>();
+
+        for (int k = 0; k < nums.length; k++) {
+            if (!obj.containsKey(nums[k])) {
+                obj.put(nums[k], new ArrayList<>());
+            }
+            obj.get(nums[k]).add(k);
+        }
+
+        Arrays.sort(nums);
+
+        List<Integer> store = new ArrayList<>();
+        int i = 0;
+        int j = nums.length - 1;
+
+        while (i < j) {
+            if (nums[i] + nums[j] == target) {
+                store.add(nums[i]);
+                store.add(nums[j]);
+                break;
+            } else if (nums[i] + nums[j] < target) {
+                i++;
+            } else {
+                j--;
             }
         }
+
+        int[] res = new int[2];
+        for (int k = 0; k < 2; k++) {
+            res[k] = obj.get(store.get(k)).remove(obj.get(store.get(k)).size() - 1);
+        }
+
         return res;
     }
 }
+
