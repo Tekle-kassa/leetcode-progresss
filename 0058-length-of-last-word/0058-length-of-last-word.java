8324b71d5e1c9eaa66b0ad86247a468c4234bedf
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-10-26 16:43:22 +0300

    Time: 0 ms (100.00%), Space: 40.7 MB (39.31%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,17 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        char space=' ';
         int count=0;
         int i=s.length()-1;
         
         while(i>=0) {
-            if(s.charAt(i)==space) {
+            if(s.charAt(i)==32){
                 i--;
                 continue;
             }
             
             while(i>=0){ 
-                if(s.charAt(i)==space){
+                if(s.charAt(i)==32){
                     return count;
                 }
                 count++;
