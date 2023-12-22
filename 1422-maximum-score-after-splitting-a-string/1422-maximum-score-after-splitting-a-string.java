12cca26bb4ce59b09d4450ad72dc967d90f1d365
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2023-12-22 11:15:40 +0300

    Time: 1 ms (97.78%), Space: 41.8 MB (32.44%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -1,21 +1,23 @@
 class Solution {
     public int maxScore(String s) {
-        int result=0;
-        for(int i=0;i<s.length()-1;i++){
-            int temp=0;
-            for(int j=0;j<=i;j++){
-                if(s.charAt(j)=='0'){
-                    temp++;
-                }
-                
+        int ones=0;
+        int ans=0;
+        int zeros=0;
+        int i=0;
+        while(i<s.length()){
+            if(s.charAt(i)=='1'){
+                ones++;
             }
-            for(int j=i+1;j<s.length();j++){
-                if(s.charAt(j)=='1'){
-                    temp++;
-                }
+            i++;
+        }   
+        for(int j=0;j<s.length()-1;j++){
+            if(s.charAt(j)=='0'){
+                zeros++;
+            }else{
+                ones--;
             }
-            result=Math.max(result,temp);
+            ans=Math.max(ans,zeros+ones);
         }
-        return result;
+        return ans;
     }
 }
