db16643fc5d5fc5eea8b5e8b539fc95ebea0df51
Tekle-kassa <101509249+Tekle-kassa@users.noreply.github.com>
2024-02-22 15:15:47 +0300

    Time: 601 ms (68.27%), Space: 21.6 MB (86.28%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        trust_count = defaultdict(int)  
+        
+        for a, b in trust:
+            trust_count[a] -= 1  
+            trust_count[b] += 1  
+        
+        for i in range(1, n + 1):
+            if trust_count[i] == n - 1:
+                return i  
+        
+        return -1 
